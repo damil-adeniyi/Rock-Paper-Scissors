fbcb13c24aa2316547037f643277c09a0a59037a
Damil Adeniyi <damiladeniyi301@gmail.com>
2025-12-11 10:14:16 -0500

    create the playRound function and place it in the playGame function

--- script.js
+++ script.js
@@ -3,17 +3,52 @@
 function getComputerChoice () {
     const choices = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * choices.length);
-    let randomChoice = choices[randomIndex];
-    return randomChoice
+    let computerChoice = choices[randomIndex];
+    return computerChoice
     // console.log(randomChoice);   
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt('Pick your choice between Rock, Paper or Scissors');
+    let humanChoice = prompt('Pick your choice between rock, paper or scissors');
     return humanChoice
     // console.log(humanChoice);    
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+
+
+function playGame() {
+
+    function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice) {
+        console.log('A draw');
+        
+    }else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`); 
+    }else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`); 
+    }else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beat ${humanChoice}`); 
+    }else{
+        humanScore++;
+        console.log(`You Win! ${humanChoice} beat ${computerChoice}`);
+    }
+   
+}
+
+playRound()
+playRound()
+playRound()
+playRound()
+playRound()
+
+}
 
